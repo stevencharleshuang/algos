cac1a93f89834fb071873ccb5e48535ea779a7e8
Steven Huang <steven.charles.huang@gmail.com>
2019-03-10 11:38:10 -0400

    added new hr algo. added new brilliant.org algos section. solved brilliant string algo

---
+++ brilliant/longest-substring.js
@@ -0,0 +1,35 @@
+/**
+ * Longest Substring
+ * https://brilliant.org/practice/strings-intermediate/?subtopic=types-and-data-structures&chapter=strings
+ * 
+ * @function longestSubstring
+ * @description accepts a string and determines the length of the longest substring that doesn't repeat letters
+ * @param {string} s: a string to analyze
+ * @returns {number} subLength: the length of the longest substring with unique characters
+ */
+const longestSubstring = (s) => {
+  let tempArr = [];
+  let longestSubSLen = 0;
+
+  for (let i = 0; i < s.length; i += 1) {
+    if (tempArr.indexOf(s[i]) === -1) {
+      tempArr.push(s[i]);
+    } else {
+      tempArr.push(s[i]);
+
+      if (tempArr.length > longestSubSLen) {
+        longestSubSLen = tempArr.length;
+      }
+      
+      tempArr = [];
+    }
+  }
+
+  return longestSubSLen;
+}
+
+let testCase;
+
+testCase = 'brilliant'; // => liant => 5
+testCase = 'xhbeqirxwobpuhkojsijumtfhsvyhyznuvzooiqxkvllfrpfnweiucjilnwixlucopomethoczbujltfycvbvvhuzstnmjcqgqchrktvsinunbopmgbwyegwkysmcxsdlhsbtcczfvcfvrbsqsxliyxzgzwvwgvvvgxgqyrbfetiwyqyircnysvcpfywdihnkjhwjsww';
+console.log(longestSubstring(testCase));
---
+++ hacker-rank/jim-and-the-orders.js
@@ -0,0 +1,23 @@
+/**
+ * Jim and the Orders
+ * https://www.hackerrank.com/challenges/jim-and-the-orders/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+ * 
+ * @function jimOrders
+ * @description It should return an array of integers that represent the order that customers' orders are delivered.
+ * @param {array} orders: a 2D integer array where each  is in the form .
+ * @returns {array} a single line of n space-separated customer numbers (recall that customers are numbered from 1 to n) that describes the sequence in which the customers receive their burgers. If two or more customers receive their burgers at the same time, print their numbers in ascending order.
+ */
+
+function jimOrders(orders) {
+
+
+}
+
+let testCase;
+testCase = [[1, 3], [2, 3], [3, 3]]; // => [ 1, 2, 3 ];
+testCase = [[8, 1], [4, 2], [5, 6], [3, 1], [4, 3]]; // => [4, 2, 5, 1, 3];
+
+/* Pseudo Code
+
+*/
+
