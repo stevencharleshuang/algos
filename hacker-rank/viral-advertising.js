8eba521f890b2b4b9d86a5c180d4a480d491269e
Steven Huang <steven.charles.huang@gmail.com>
2019-03-20 12:11:36 -0400

    solved HR viral-advertising algo

---
+++ hacker-rank/viral-advertising.js
@@ -0,0 +1,46 @@
+/**
+ * Viral Advertising
+ * https://www.hackerrank.com/challenges/strange-advertising/problem
+ * 
+ * @function viralAdvertising
+ * @description It should return the cumulative number of people who have liked the ad at a given time.
+ * @param {number} n: denoting a number of days
+ * @returns Print the number of people who liked the advertisement during the first n days.
+ */
+
+/* 
+More Explicit Solution
+*/
+// function viralAdvertising(n) {
+//   let shared = 5;
+//   let cumulative = 2;
+
+//   for (let i = 1; i < n; i += 1) {
+//     shared = Math.floor(shared / 2) * 3;
+//     cumulative += Math.floor(shared / 2);
+//   }
+  
+//   return cumulative;
+// }
+
+function viralAdvertising(n) {
+  let shared = 5;
+  let cumulative = 2;
+
+  for (let i = 1; i < n; i += 1) {
+    cumulative += Math.floor(Math.floor(shared / 2) * 3 / 2);
+  }
+
+  return cumulative;
+}
+
+let testCase;
+
+testCase = 3; // => 9
+testCase = 4; // => 15
+
+console.log(viralAdvertising(testCase));
+
+/* Pseudo Code 
+floor(5/2) * 3
+*/
